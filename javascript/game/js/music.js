e9ede8878ac40d83d78470f60d8f02ed2befb840
dkdlelw <1346zany@gmail.com>
2023-04-26 17:41:14 +0900

    0426

--- javascript/game/js/music.js
+++ javascript/game/js/music.js
@@ -57,21 +57,16 @@ const musicName = musicWrap.querySelector(".music__control .title h3");
 const musicArtist = musicWrap.querySelector(".music__control .title p");
 const musicView = musicWrap.querySelector(".music__view .image img");
 const musicAudio = musicWrap.querySelector("#main-audio");
-
-let musicIndex = 2;     //현재 음악 인덱스
-
-//음악 재생
+let musicIndex = 1;     //현재 음악 인덱스
+// 음악 재생
 const loadMusic = (num) => {
-    musicName.innerText = allMusic[num-1].name; //뮤직 이름
-    musicArtist.innerText = allMusic[num-1].artist; //뮤직 아티스트
-    musicView.src = `img/${allMusic[num-1].img}.png`;   //뮤직 이미지
-    musicView.alt = allMusic[num-1].name; //뮤직 이미지 alt
-    musicAudio.src = `audio/${allMusic[num-1].audio}.mp3`; //뮤직 파일
-
+    musicName.innerText = allMusic[num-1].name;             //뮤직 이름
+    musicArtist.innerText = allMusic[num-1].artist;         //뮤직 아티스트
+    musicView.src = `img/${allMusic[num-1].img}.png`;       //뮤직 이미지
+    musicView.alt = allMusic[num-1].name;                   //뮤직 이미지 alt
+    musicAudio.src = `audio/${allMusic[num-1].audio}.mp3`;  //뮤직 파일
 }
-
 window.addEventListener("load", () => {
     loadMusic(musicIndex);
-
     musicAudio.play();
-})
+});

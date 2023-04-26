c8e389e40c6261b80f0ee6d42133e647637ad181
dkdlelw <1346zany@gmail.com>
2023-04-26 17:33:46 +0900

    0426

--- javascript/game/js/music.js
+++ javascript/game/js/music.js
@@ -1,53 +1,77 @@
 const allMusic = [
     {
-        name : "1.음악제목",
+        name : "1.PixelAffection",
         artist : "Yeule",
         img: "music_view01",
         audio : "music_audio01"
     }, {
-        name : "2.음악제목",
-        artist : "Yeule",
-        img: "music_view01",
+        name : "2.The Future is Now",
+        artist : "POiSON GiRL FRiEND",
+        img: "music_view02",
         audio : "music_audio01"
     }, {
         name : "3.음악제목",
         artist : "Yeule",
-        img: "music_view01",
+        img: "music_view03",
         audio : "music_audio01"
     }, {
         name : "4.음악제목",
         artist : "Yeule",
-        img: "music_view01",
+        img: "music_view04",
         audio : "music_audio01"
     }, {
         name : "5.음악제목",
         artist : "Yeule",
-        img: "music_view01",
+        img: "music_view05",
         audio : "music_audio01"
     }, {
         name : "6.음악제목",
         artist : "Yeule",
-        img: "music_view01",
+        img: "music_view06",
         audio : "music_audio01"
     }, {
         name : "7.음악제목",
         artist : "Yeule",
-        img: "music_view01",
+        img: "music_view07",
         audio : "music_audio01"
     }, {
         name : "8.음악제목",
         artist : "Yeule",
-        img: "music_view01",
+        img: "music_view08",
         audio : "music_audio01"
     }, {
         name : "9.음악제목",
         artist : "Yeule",
-        img: "music_view01",
+        img: "music_view09",
         audio : "music_audio01"
     }, {
         name : "10.음악제목",
         artist : "Yeule",
-        img: "music_view01",
+        img: "music_view10",
         audio : "music_audio01"
     }
-]
+];
+
+const musicWrap = document.querySelector(".music__wrap");
+const musicName = musicWrap.querySelector(".music__control .title h3");
+const musicArtist = musicWrap.querySelector(".music__control .title p");
+const musicView = musicWrap.querySelector(".music__view .image img");
+const musicAudio = musicWrap.querySelector("#main-audio");
+
+let musicIndex = 2;     //현재 음악 인덱스
+
+//음악 재생
+const loadMusic = (num) => {
+    musicName.innerText = allMusic[num-1].name; //뮤직 이름
+    musicArtist.innerText = allMusic[num-1].artist; //뮤직 아티스트
+    musicView.src = `img/${allMusic[num-1].img}.png`;   //뮤직 이미지
+    musicView.alt = allMusic[num-1].name; //뮤직 이미지 alt
+    musicAudio.src = `audio/${allMusic[num-1].audio}.mp3`; //뮤직 파일
+
+}
+
+window.addEventListener("load", () => {
+    loadMusic(musicIndex);
+
+    musicAudio.play();
+})
